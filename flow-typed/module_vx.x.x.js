b54536239fa6dce7b44de7cf056a322944f6358e
Rizal Ibnu <muhamadrizalibnuabdulah@gmail.com>
2018-07-15 13:18:16 +0700

    Add flow config and declare initial module in flow

---
+++ flow-typed/module_vx.x.x.js
@@ -0,0 +1,51 @@
+declare module 'axios' {
+  declare module.exports: any;
+}
+declare module 'classnames' {
+  declare module.exports: any;
+}
+declare module 'helmet' {
+  declare module.exports: any;
+}
+declare module 'react-helmet' {
+  declare module.exports: any;
+}
+declare module 'react-hot-loader' {
+  declare module.exports: any;
+}
+declare module 'react-html-parser' {
+  declare module.exports: any;
+}
+declare module 'react-lazy' {
+  declare module.exports: any;
+}
+declare module 'react-loadable' {
+  declare module.exports: any;
+}
+declare module 'react-loadable-visibility' {
+  declare module.exports: any;
+}
+declare module 'react-redux' {
+  declare module.exports: any;
+}
+declare module 'react-router' {
+  declare module.exports: any;
+}
+declare module 'react-router-dom' {
+  declare module.exports: any;
+}
+declare module 'recompose' {
+  declare module.exports: any;
+}
+declare module 'redux' {
+  declare module.exports: any;
+}
+declare module 'redux-devtools-extension' {
+  declare module.exports: any;
+}
+declare module 'redux-promise-middleware' {
+  declare module.exports: any;
+}
+declare module 'redux-thunk' {
+  declare module.exports: any;
+}
